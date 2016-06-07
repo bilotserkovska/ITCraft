f5782c0e710a0c3de6cea61cd0fbb4a6b6a16c24
bilotserkovska <belotserkovskaya0903@gmail.com>
2016-06-07 21:03:40 +0300

    animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
 	$('.carousel').carousel()
+	$('figure').on('inview', function(event, isInView) {
+		$('.sign-left').animate({
+				opacity: 1
+			},800, function(){
+				$('.sign-center').animate({
+					opacity: 1
+				},800, function(){
+					$('.sign-right').animate({
+						opacity: 1
+					},800);
+				});
+		});			
+
+	});
 });
